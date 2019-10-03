583df206510f0b4aa99f567d79bd7169e819beb9
jordan van beijnhem <jordan.van.beijnhem@hva.nl>
2019-10-03 17:27:50 +0200

    Setup app for tomcat

--- src/main/kotlin/nl/jordanvanbeijnhem/sudoku/SudokuApplication.kt
+++ src/main/kotlin/nl/jordanvanbeijnhem/sudoku/SudokuApplication.kt
@@ -2,9 +2,17 @@ package nl.jordanvanbeijnhem.sudoku
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.boot.builder.SpringApplicationBuilder
+
 
 @SpringBootApplication
-class SudokuApplication
+class SudokuApplication : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(SudokuApplication::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SudokuApplication>(*args)
